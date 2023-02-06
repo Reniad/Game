40e7887f6eccf9c16f5483b4fb8a0bcb9dacb57a
Reniad <reniadi@gmail.com>
2023-02-06 18:57:49 +0100

    Done by me

--- main.js
+++ main.js
@@ -1,76 +1,137 @@
-const choices = ["rock","paper","scissors"];
-const winners=[];
-
-
-function game(){
-    alert('In this Game you will be playing 5 rounds at once !Please Try to type Correctly ! Good luck! ');
-    alert('Ready ? ....Go !!!');
-  for(let i = 0; i<5 ; i++){
-    playRound(i);
-  }
-  Winners();
-}
-
-function playRound(round){
- const playerSelection = playerChoice();
- const computerSelection = computerChoice();
- const winner = checkWinner(playerSelection,computerSelection);
- Round(playerSelection,computerSelection,winner,round);
-}
-
-function playerChoice (){
-  let input =prompt('type Rock , Paper or Scissors');
-  while(input == null){
-    input = prompt('Type Rock , Paper or Scissors');
-  }
-  input =input.toLowerCase();
-  let check = validateInput(input)
-  while (check == false ){
-    input =prompt('Oops Try again');
-    input=input.toLowerCase();
-    check = validateInput(input); 
-  }
-return input; 
-}
-
-function computerChoice(){
-  return choices[Math.floor(Math.random()*choices.length)];
-}
-
-function validateInput(choice){
-  if (choices.includes(choice)){
-    return true; 
-  } else {
-    return false;
-  }
-}
-
-function checkWinner(ChoicePlayer,ChoiceComputer){
-  if(ChoicePlayer === ChoiceComputer){
-    return 'Equality';
-  } else if ((ChoicePlayer ==='rock' && ChoiceComputer=='scissors') || (ChoicePlayer ==='paper' && ChoiceComputer=='rock') || (ChoicePlayer ==='scissors' && ChoiceComputer=='paper')){
-    return 'Player ';
-  } else {
-    return 'Computer '
-  }
-}
-
-function Winners(){
-  let playerWins = winners.filter((item) => item == "Player").length;
-  let computerWins = winners.filter((item) => item == "Computer").length;
-  let equality = winners.filter((item) => item == "equality").length;
-  console.log('Results :');
-  console.log('Player :',playerWins);
-  console.log('Computer :',computerWins);
-  console.log('Equality:',equality);
-}
-
-function Round(playerChoice,computerChoice,winner,round){
-  console.log('Round:',round);
-  console.log('Player choice:',playerChoice);
-  console.log('Computer choice:',computerChoice);
-  console.log(winner,'won the round');
-  console.log("---------------------");
-}
-
+const gameValues = ["rock", "paper", "scissors"];
+let cancelledByUser = false;
+let finished = false;
+let userScore = 0;
+let computerScore = 0;
+const computerDecision = () => Math.floor(Math.random() * 3);
 game();
+
+
+function handleCancel(origin) {
+    let cancelGame = confirm('Click Ok to confirm you want to leave the game');
+    if (cancelGame) {
+        cancelledByUser = true;
+        return
+    } else if (origin === 'game') {
+        game();
+    }
+}
+
+function getUserOption(callback) {
+    let userSelection = prompt('Enter your choice, rock, paper or scissors');
+    if (userSelection === null) {
+        handleCancel("getUserOption");
+        if (cancelledByUser) {
+            finishGame();
+        } else {
+            callback
+        }
+    }
+    if (cancelledByUser) {
+        finished = true;
+        return "game over";
+    }
+    if (userSelection !== null && gameValues.includes(userSelection.toLowerCase().trim())) {
+        return userSelection.toLowerCase().trim();
+    } else {
+        if (userSelection !== null) {
+            if (userSelection === "") {
+                alert("Please type an option: 'rock','paper' or 'scissors")
+            } else {
+                alert(`${userSelection} is not a valid option, please write 'rock','paper' or 'scissors'`);
+            }
+        }
+        callback
+    }
+}
+
+function singleRound(computerOption, userOption) {
+    if (userOption === gameValues[computerOption]) return "draw";
+    switch (userOption) {
+        case 'rock':
+            if (gameValues[computerOption] === "paper") {
+                return "computer"
+            } else {
+                return "user"
+            };
+        case 'paper':
+            if (gameValues[computerOption] === "scissors") {
+                return "computer"
+            } else {
+                return "user"
+            };
+        case 'scissors':
+            if (gameValues[computerOption] === "scissors") {
+                return "computer"
+            } else {
+                return "user"
+            };
+        case 'game over':
+            return 'game over';
+    }
+}
+function game() {
+    let start = confirm('ROCK, PAPER, SCISSORS GAME\nYou only have to write rock, paper o scissors to play.\nThere are five rounds, draws count as a round.\nShall we start??');
+    if (!start) {
+        handleCancel('game');
+    } else {
+        alert('You can use Ctrl + Shift + I or right click and inspect at the end of the game to open the console and see game details.')
+        let counter = 1;
+        while (counter <= 5) {
+            let computerOption = computerDecision();
+            let userOption = getUserOption();
+            let handResult = singleRound(computerOption, userOption);
+            switch (handResult) {
+                case "computer":
+                    alert(`Computer chose ${gameValues[computerOption]}\nYou loose round ${counter}`);
+                    console.log(`Computer chose ${gameValues[computerOption]}\nYou loose round ${counter}`);
+                    counter++;
+                    computerScore++
+                    break
+                case "user":
+                    alert(`Computer chose ${gameValues[computerOption]}\nYou win round ${counter}`);
+                    console.log(`Computer chose ${gameValues[computerOption]}\nYou win round ${counter}`);
+                    counter++;
+                    userScore++;
+                    break
+                case "draw":
+                    alert(`Computer chose ${gameValues[computerOption]}\nYou draw round ${counter}`);
+                    console.log(`Computer chose ${gameValues[computerOption]}\nYou draw round ${counter}`);
+                    counter++;
+                    break
+                case "game over":
+                    counter = 6;
+                    break
+            }
+        }
+        finishGame();
+    }
+}
+
+function finishGame() {
+    if (!finished) {
+
+        if (cancelledByUser) {
+            alert('You can try again whenever you want');
+            console.log('You can try again whenever you want');
+        } else {
+            if (userScore > computerScore) {
+                alert(`You win ${userScore} to ${computerScore}`);
+                console.log(`You win ${userScore} to ${computerScore}`);
+            } else if (userScore < computerScore) {
+                alert(`You lost ${userScore} to ${computerScore}`);
+                console.log(`You lost ${userScore} to ${computerScore}`);
+            } else {
+                alert(`You draw ${userScore} to ${computerScore}`);
+                console.log(`You draw ${userScore} to ${computerScore}`);
+            }
+            let playAgain = confirm('Do you want to play again??');
+            if (playAgain) {
+                game();
+            } else {
+                alert("We can't wait to see you again!!!!")
+            }
+        }
+        alert('End of the game!!!!!!!')
+    }
+}
